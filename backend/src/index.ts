0b397f3bc104d628b19fad71dd90295dfa8d64ca
HarryMumford <harry_mumford@hotmail.co.uk>
2020-08-18 12:56:11 +0100

    feat: install dotenv and hide sensitive data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,25 @@
-import app from './application';
+import app from './application'
+import { createConnection } from 'mysql'
+import * as dotenv from 'dotenv'
 
-const port = process.env.PORT || 8000;
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+const connection = createConnection({
+  host: process.env.DB_HOST,
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'birdietest'
+})
+
+connection.connect((err) => {
+  if (err) throw err
+  console.log('Connected!')
+})
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
-  console.log(`Server started at http://localhost:${port}`);
-});
+  console.log(`Server started at http://localhost:${port}`)
+})
